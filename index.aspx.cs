c715736d9b7369fdfdfe3812285421557b4e1049
ethanx94 <ethanx94@gmail.com>
2016-02-24 14:15:32 -0600

    started aspx project

---
+++ index.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Clear the last session for adding additional entries
+        Session.Clear();
+    }
+}
