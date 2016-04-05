e10df957f7d753d6ba58f668103659148460ae17
allisonanne <allison.campbell@siu.edu>
2016-04-04 19:41:23 -0500

    Add sendgrid with email after registration

--- WebApplication1/Account/Register.aspx.cs
+++ WebApplication1/Account/Register.aspx.cs
@@ -4,8 +4,8 @@ using System.Web;
 using System.Web.UI;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
-using Owin;
 using WebApplication1.Models;
+using System.Net.Mail;
 
 namespace WebApplication1.Account
 {
@@ -24,6 +24,14 @@ namespace WebApplication1.Account
                 //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
                 //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
 
+                var myMessage = new SendGrid.SendGridMessage();
+                myMessage.AddTo(Email.Text);
+                myMessage.From = new MailAddress("allison.campbell282@gmail.com", "ToDoTaskList");
+                myMessage.Subject = "Welcome to ToDoTaskList!";
+                myMessage.Text = "ToDoTaskList is an iterative tasklist that helps you seize the day.";
+
+                var transportWeb = new SendGrid.Web("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");
+                transportWeb.DeliverAsync(myMessage);
                 signInManager.SignIn( user, isPersistent: false, rememberBrowser: false);
                 IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
             }
